152f502fa9e481e6ab98f48530d4afed97258bd6
rmcc0724 <rmcc0724@gmail.com>
2018-11-26 21:10:07 -0800

    Update count.js

--- count.js
+++ count.js
@@ -8,10 +8,8 @@ var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-var newNow = days + "days " + hours + "h "
+return days + "days " + hours + "h "
     + minutes + "m " + seconds + "s ";
-
-  return newNow;
   } 
   
   render() {
